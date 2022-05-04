b8728473fea743fd5fafe60ee91d50edda938461
Rosa Regalado <rosaregalado101@gmail.com>
2022-05-03 23:38:37 -0700

    users can submit & save posts

--- server.js
+++ server.js
@@ -2,16 +2,24 @@
 const express = require('express');
 const app = express();
 
+require('./data/reddit-db');
+require('./controllers/posts')(app);
+
 // middleware
 const exphbs  = require('express-handlebars');
 app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // routes
 app.get('/', (req, res) => {
   res.render('home');
-})
+});
 
+app.get('/posts/new', (req, res) => {
+  res.render('posts-new');
+});
 
 
 // Start server
